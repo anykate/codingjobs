df7db359ac18441572e6b5354aea32444bec85a2
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-11-02 14:15:01 +0530

    Video - Part01

---
+++ allapps/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html', {})
+
+
+def signup(request):
+    return render(request, 'core/signup.html', {})
